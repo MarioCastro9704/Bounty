e18c6db6d1cbbdaa5b69f269fefc89c1d3428d88
Eleazar Estrada <elestradariffo@gmail.com>
2024-06-07 12:36:45 -0500

    Ajustes en el navbar y button para crear un nuevo producto

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def user_color(user)
+    colors = ['#FF5733', '#33FF57', '#3357FF', '#F0A500', '#A500F0', '#00F0A5']
+    index = user.email[0].ord % colors.length
+    colors[index]
+  end
 end
