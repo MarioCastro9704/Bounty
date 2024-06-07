9d56bda72e83feb939d51c740b7e47f83c39b9e5
Eleazar Estrada <elestradariffo@gmail.com>
2024-06-07 14:07:23 -0500

    Ajustes visuales en el navbar y en crear un nuevo producto

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @latest_releases = Product.order(created_at: :desc).limit(9)
-    @recently_added = Product.order(created_at: :desc).limit(9)
-    @best_sellers = Product.joins(:purchases).group('products.id').order('COUNT(purchases.id) DESC').limit(9)
+    @latest_releases = Product.order(created_at: :desc).limit(6)
+    @recently_added = Product.order(created_at: :desc).limit(6)
+    @best_sellers = Product.joins(:purchases).group('products.id').order('COUNT(purchases.id) DESC').limit(6)
   end
 end
