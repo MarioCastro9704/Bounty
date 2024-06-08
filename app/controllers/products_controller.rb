31d837e0f80435ed14564a1fee0fa2289d29e5ef
Eleazar Estrada <elestradariffo@gmail.com>
2024-06-07 23:57:24 -0500

    modal para eliminar productos. Todo OK

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,7 +29,7 @@ class ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
-      redirect_to @product, notice: 'Producto actualizado exitosamente.'
+      redirect_to products_path, notice: 'Producto actualizado exitosamente.'
     else
       render 'edit'
     end
