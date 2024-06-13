09e73c5c9a8b13329feac4cd68646fa1eb4e2e93
Eleazar Estrada <elestradariffo@gmail.com>
2024-06-13 18:07:21 -0500

    ajustes en el home

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,11 @@ default_category = Category.find_or_create_by!(name: 'Default')
 
 # Asignar la categoría predeterminada a productos que no tienen categoría
 Product.where(category_id: nil).update_all(category_id: default_category.id)
+categories = ["Zapatillas", "Remeras", "Pantalones", "Accesorios"]
+categories.each do |category_name|
+  Category.create!(name: category_name)
+end
+# Crear una categoría predeterminada si no existe
+default_category = Category.find_or_create_by!(name: 'Default')
+# Asignar la categoría predeterminada a productos que no tienen categoría
+Product.where(category_id: nil).update_all(category_id: default_category.id)
